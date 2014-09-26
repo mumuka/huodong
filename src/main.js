65952ad242848667ee3c420463246de06e413418
hldzlk <zhoulink90@gmail.com>
2014-09-26 09:51:18 +0800

    报名列表1阶段
    报名列表1阶段

--- src/main.js
+++ src/main.js
@@ -1,21 +1,22 @@
 requirejs.config({
 	baseUrl:"src",
 	paths:{
-		jquery:"//dev.comeoncloud.net/test/lib/jquery/jquery-2.1.1.min"
+		jquery:"http://dev.comeoncloud.net/test/lib/jquery/jquery-2.1.1.min"
 	}
 })
+
 requirejs(["jquery"],function($){
 
-	$(".weixinshare").bind("click",function(){
+	$("#baoming").bind("click",function(){
 		$(".sendinfobox").css({"-webkit-transform":"translate(0,0px)"})
-		$(".weixinshare").css({"-webkit-transform":"translate(0,49px)"})
+		$("#baoming").css({"-webkit-transform":"translate(0,49px)"})
 		$(".mainbtn").css({"-webkit-transform":"translate(0,-49px)"})
 	})
 
 	$(".closebtn").bind("click",function(){
-
 		$(".sendinfobox").css({"-webkit-transform":"translate(0,300px)"})
-		$(".weixinshare").css({"-webkit-transform":"translate(0,0px)"})
+		$("#baoming").css({"-webkit-transform":"translate(0,0px)"})
 		$(".mainbtn").css({"-webkit-transform":"translate(0,0px)"})
 	})
+
 })
